fd8a1b9551335d87899d117e6e54f038faa9da7e
Marius Goetze <marius-goetze@arcor.de>
2023-03-19 20:02:51 +0100

    implemented (memory) storage

--- internal/infrastructure/memstorage/computer.go
+++ internal/infrastructure/memstorage/computer.go
@@ -4,16 +4,36 @@ import "github.com/marius-go/computer-management-service/internal/core/domain"
 
 type computer struct {
 	Name                 string
-	MAC                  *string
-	IP                   *string
-	EmployeeAbbreviation *string
-	Description          *string
+	MAC                  string
+	IP                   string
+	EmployeeAbbreviation string
+	Description          string
 }
 
 func (c computer) ToDomain() domain.Computer {
-	return domain.Computer(c)
+	return domain.Computer{
+		Name:                 c.Name,
+		MAC:                  &c.MAC,
+		IP:                   &c.IP,
+		EmployeeAbbreviation: &c.EmployeeAbbreviation,
+		Description:          &c.Description,
+	}
 }
 
 func ComputerFromDomain(comp domain.Computer) computer {
-	return computer(comp)
+	compStorage := computer{Name: comp.Name}
+	if comp.MAC != nil {
+		compStorage.MAC = *comp.MAC
+	}
+	if comp.IP != nil {
+		compStorage.IP = *comp.IP
+	}
+	if comp.EmployeeAbbreviation != nil {
+		compStorage.EmployeeAbbreviation = *comp.EmployeeAbbreviation
+	}
+	if comp.Description != nil {
+		compStorage.Description = *comp.Description
+	}
+
+	return compStorage
 }
