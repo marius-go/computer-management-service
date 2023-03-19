7a375941282455cd864781dddf02370ba0115243
Marius Goetze <marius-goetze@arcor.de>
2023-03-19 20:04:39 +0100

    implemented service

--- internal/core/domain/computer.go
+++ internal/core/domain/computer.go
@@ -1,9 +1,30 @@
 package domain
 
+import "fmt"
+
+const LenEmployeeAbbreviation int = 3
+
 type Computer struct {
 	Name                 string
 	MAC                  *string
 	IP                   *string
 	EmployeeAbbreviation *string
 	Description          *string
+}
+
+func (c *Computer) Validate() error {
+	if c.Name == "" {
+		return ErrValidation("property 'Name' is required")
+	}
+	if c.MAC == nil || *c.MAC == "" {
+		return ErrValidation("property 'MAC' is required")
+	}
+	if c.IP == nil || *c.IP == "" {
+		return ErrValidation("property 'IP' is required")
+	}
+	if c.EmployeeAbbreviation != nil && len(*c.EmployeeAbbreviation) > 0 && len(*c.EmployeeAbbreviation) != LenEmployeeAbbreviation {
+		return ErrValidation(fmt.Sprint("if set, 'EmployeeAbbreviation' must be exactly ", LenEmployeeAbbreviation, " characters long"))
+	}
+
+	return nil
 }
--- internal/core/service/computer/computer_service.go
+++ internal/core/service/computer/computer_service.go
@@ -1,34 +1,151 @@
 package computer
 
 import (
+	"errors"
+	"fmt"
+	"log"
+
 	"github.com/marius-go/computer-management-service/internal/core/domain"
 	"github.com/marius-go/computer-management-service/internal/core/port"
 )
 
+const assignedComputersThreshold int = 3
+
 type Service struct {
-	storage port.ComputerStorage
+	storage       port.ComputerStorage
+	adminNotifier port.AdminNotification
 }
 
-func New(storage port.ComputerStorage) *Service {
-	return &Service{storage: storage}
+func New(storage port.ComputerStorage, adminNotifier port.AdminNotification) *Service {
+	return &Service{storage: storage, adminNotifier: adminNotifier}
 }
 
-func (s *Service) CreateComputer(domain.Computer) (domain.Computer, error) {
-	return domain.Computer{}, nil
+func (s *Service) CreateComputer(computer domain.Computer) (domain.Computer, error) {
+	err := computer.Validate()
+	if err != nil {
+		return domain.Computer{}, err
+	}
+
+	_, err = s.storage.GetComputer(computer.Name)
+	// only continue if resource is not found
+	if err == nil {
+		return domain.Computer{}, domain.ErrConflict("Name")
+	} else if !errors.Is(err, domain.ErrNotFound) {
+		return domain.Computer{}, fmt.Errorf("storage error: %w", err)
+	}
+
+	comp, err := s.storage.CreateComputer(computer)
+	if err != nil {
+		return domain.Computer{}, annotateStorageError(err)
+	}
+
+	// check for assignments only after the computer is added, due to races we could get to many notifications,
+	// but when checking before adding, notifications events could be missed due to those races
+	if computer.EmployeeAbbreviation != nil && *computer.EmployeeAbbreviation != "" {
+		s.checkAssignments(*computer.EmployeeAbbreviation)
+	}
+
+	return comp, nil
 }
 
-func (s *Service) UpdateComputer(domain.Computer) (domain.Computer, error) {
-	return domain.Computer{}, nil
+func (s *Service) UpdateComputer(update domain.Computer) (domain.Computer, error) {
+
+	computer, err := s.storage.GetComputer(update.Name)
+	if err != nil {
+		return domain.Computer{}, annotateStorageError(err)
+	}
+
+	updateComputer(&computer, update)
+
+	err = computer.Validate()
+	if err != nil {
+		return domain.Computer{}, err
+	}
+
+	computer, err = s.storage.UpdateComputer(computer)
+	if err != nil {
+		return domain.Computer{}, annotateStorageError(err)
+	}
+
+	// check for assignments only after the computer was updated, due to races we could get to many notifications,
+	// but when checking before updated, notifications events could be missed due to those races
+	if update.EmployeeAbbreviation != nil && *update.EmployeeAbbreviation != "" {
+		s.checkAssignments(*update.EmployeeAbbreviation)
+	}
+
+	return computer, nil
 }
 
 func (s *Service) DeleteComputer(name string) error {
+	_, err := s.storage.GetComputer(name)
+	if err != nil {
+		return annotateStorageError(err)
+	}
+
+	err = s.storage.DeleteComputer(name)
+	if err != nil {
+		return annotateStorageError(err)
+	}
+
 	return nil
 }
 
 func (s *Service) GetComputer(name string) (domain.Computer, error) {
-	return domain.Computer{}, nil
+	computer, err := s.storage.GetComputer(name)
+	if err != nil {
+		return domain.Computer{}, annotateStorageError(err)
+	}
+
+	return computer, nil
 }
 
-func (s *Service) ListComputers(employeeAbbreviation string) ([]domain.Computer, error) {
-	return nil, nil
+func (s *Service) ListComputers(employeeAbbreviation *string) ([]domain.Computer, error) {
+	computers, err := s.storage.ListComputers(employeeAbbreviation)
+	if err != nil {
+		return nil, annotateStorageError(err)
+	}
+
+	return computers, nil
+}
+
+func (s *Service) checkAssignments(employeeAbbreviation string) {
+	assignedComputers, err := s.storage.ListComputers(&employeeAbbreviation)
+	if err != nil {
+		log.Println("could not check assignments due to storage error:", err)
+		return
+	}
+
+	numAssignedComputers := len(assignedComputers)
+	if numAssignedComputers >= assignedComputersThreshold {
+		message := fmt.Sprint("employee has ", numAssignedComputers, " computers assigned")
+		err := s.adminNotifier.Notify("warning", employeeAbbreviation, message)
+		if err != nil {
+			log.Println("could not notify due to notification service error:", err, "; dropped notification:", message, "("+employeeAbbreviation+")")
+		}
+	}
+}
+
+// updates `computer` with the set values of `update`
+func updateComputer(computer *domain.Computer, update domain.Computer) {
+	if update.MAC != nil {
+		*computer.MAC = *update.MAC
+	}
+	if update.IP != nil {
+		*computer.IP = *update.IP
+	}
+	if update.EmployeeAbbreviation != nil {
+		*computer.EmployeeAbbreviation = *update.EmployeeAbbreviation
+	}
+	if update.Description != nil {
+		*computer.Description = *update.Description
+	}
+}
+
+func annotateStorageError(err error) error {
+	switch {
+	case errors.Is(err, domain.ErrNotFound):
+		return err
+	default:
+		return fmt.Errorf("storage error: %w", err)
+	}
 }
